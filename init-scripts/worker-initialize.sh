2277d0c3a5fc09423a0b695ba4b25a5c5cecd905
George Fiotakis <g.fiotakis@hotmail.com>
2020-04-15 15:24:09 +0200

    Add certificate

--- init-scripts/worker-initialize.sh
+++ init-scripts/worker-initialize.sh
@@ -95,11 +95,11 @@ fi
 
 # Now create a new directory for docker certificate and copy the Root CA certificate into it.
 sudo mkdir -p /etc/docker/certs.d/private.registry.io/
-sudo cp docker-registry/nginx/ssl/private-registry-cert.crt /etc/docker/certs.d/private.registry.io/
+sudo cp ../depentencies/certificate/private-registry-cert.crt /etc/docker/certs.d/private.registry.io/
 
 # And then create a new directory '/usr/share/ca-certificate/extra' and copy the Root CA certificate into it.
 sudo mkdir -p /usr/share/ca-certificates/extra/
-sudo cp docker-registry/nginx/ssl/private-registry-cert.crt /usr/share/ca-certificates/extra/
+sudo cp ../depentencies/certificate/private-registry-cert.crt /usr/share/ca-certificates/extra/
 
 # Update certificates & restart docker
 sudo dpkg-reconfigure ca-certificates
