242cdbec8ae25ad55313b44ace0bd341cee4f49b
George Fiotakis <g.fiotakis@hotmail.com>
2020-10-02 23:48:59 +0200

    Fix wrong commit :D

--- docker-registry/init-registry.sh
+++ docker-registry/init-registry.sh
@@ -37,23 +37,5 @@ else
     fi
 fi
 
-echo "Checking .env file"
-if [ "$(cat .env)" ]; then
-        CURR_DIR=$(grep CURR_DIR .env | cut -d '=' -f2)
-else
-    touch .env && echo 'CURR_DIR='$PWD >> .env
-    CURR_DIR=$(grep CURR_DIR .env | cut -d '=' -f2)
-fi
-
-
-### Check if a directory does not exist ###
-if [ ! -d "$CURR_DIR/mnt/local-nfs" ] 
-then
-    echo "Directory $CURR_DIR/mnt/local-nfs DOES NOT exists." 
-    exit 9999 # die with error code 9999
-fi
-
-sudo -u root chmod -R 0444  $CURR_DIR/mnt/local-nfs
-
 echo 'all set ! firing up nfs server !'
 docker-compose up -d
