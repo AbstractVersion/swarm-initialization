f4565545e88405d6276f0838cfd5a7e472f159d4
George Fiotakis <g.fiotakis@hotmail.com>
2020-10-02 22:51:52 +0200

    Update nfs-script to install Docker & Docker compose from global docker repo

--- nfs-server/init-nfs.sh
+++ nfs-server/init-nfs.sh
@@ -1,22 +1,41 @@
 #!/bin/sh
 # initialize nfs-server
-echo 'installing nfs-common'
+echo 'installing nfs-common...'
 sudo -u root apt install nfs-common -y -qq
-# echo 'Creating .env file'
-# touch .env && echo 'CURR_DIR='$PWD >> .env
+echo 'installing git...'
+sudo -u root apt-get install git -y -qq
+
+
+install
 
 echo 'Checking docker installation...'
 if [ "$(which docker)" ]; then
     echo "Docker found!"
-    if [ "$(which docker-compose)" ]; then
-        echo "docker-compose found!"
+else
+    read -p "Do you want to install docker compose ? (y/n) " RESP
+    if [ "$RESP" = "y" ]; then
+        echo "Installing Docker for you."
+        git clone https://github.com/docker/docker-install.git
+        chmod +x docker-install/install.sh
+        sudo -u root docker-install/install.sh
     else
-        echo "please install docker-compose"
+        echo "Exiting installation."
         exit 0
     fi
+fi
+if [ "$(which docker-compose)" ]; then
+    echo "docker-compose found!"
 else
-    echo "please install docker"
-    exit 0
+    read -p "Do you want to install docker compose ? (y/n) " RESP
+    if [ "$RESP" = "y" ]; then
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+        sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+        docker-compose --version
+    else
+        echo "Exiting installation."
+        exit 0
+    fi
 fi
 
 echo "Checking .env file"
